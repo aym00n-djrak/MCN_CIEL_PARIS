367ea00403e85679975b058ac28507359e68e93f
aymoon <61629065+aym00n-djrak@users.noreply.github.com>
2023-01-19 12:55:31 +0100

    feat: photo

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import paris from "./assets/paris.png";
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Sous le ciel Paris!</Text>
+      <Image
+        source={paris}
+        style={{
+          width: 200,
+          height: 200,
+          marginTop: 20,
+          marginBottom: 200,
+          alignSelf: "center",
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
